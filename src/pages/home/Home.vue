afa2d2c01135cb89008ae5fc58f15237484abadc
Claudia Llop <sapwasap25@gmail.com>
2026-03-04 12:28:39 +0100

    márgenes de home cerrados POR FIN

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -4,12 +4,41 @@
 </script>
 
 <template>
+  <div class="contenedor">
+    <div class="contenido">
+  <p class="parrafo-introduccion">
+    Este párrafo usa Perfectly Nineties.
+  </p>
 
-<h1>Página principal</h1>
-
-
+  <p>
+    Este párrafo usa la fuente por defecto.
+  </p>
+  </div>
+  </div>
 </template>
 
 <style scoped>
+.parrafo-introduccion {
+  font-family: "perfectly-nineties-regular", sans-serif;
+  font-size: 2.5rem;
+  transition: font-size 0.3s ease-in-out;
+}
 
+.contenedor {
+  width: 100%;
+  padding: 0 10rem;
+  box-sizing: border-box;
+  margin: 2rem 0;
+}
+
+.contenido {
+  margin: 0 auto;
+}
+
+@media (min-width: 1024px) {
+  .parrafo-introduccion {
+    font-size: 3rem;
+  }
+
+}
 </style>
