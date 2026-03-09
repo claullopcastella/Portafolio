174be92c5f7c57ebab5164c57842fd743117c3c0
Claudia Llop <sapwasap25@gmail.com>
2026-03-09 16:01:22 +0100

    nueva prueba de la web

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,6 +1,13 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
 
+interface Proyecto {
+  id: number;
+  nombre: string;
+  imagen: string;
+  link: string;
+}
+
 const imagenDefault = '/imagenes/Justin-Bieber-PNG.png';
 const imagenHover = '/imagenes/Justin-Bieber-PNG-hover.png';
 const iconoMenu1 = ref(imagenDefault);
@@ -12,19 +19,37 @@ const quitarHover = () => {
   iconoMenu1.value = imagenDefault;
 }
 
-const botones = Array(6).fill(0).map((_, i) => ({
-  id: i,
-  imagen: '/imagenes/post-it-1.png',
-  hoverImagen: '/imagenes/post-it-2.png',
-  current: ref('/imagenes/post-it-1.png')
-}));
 
-const mouseEnter = (boton: any) => {
-  boton.current.value = boton.hoverImagen;
-}
-const mouseLeave = (boton: any) => {
-  boton.current.value = boton.imagen;
-}
+const proyectos: Proyecto[] = [
+  { id: 1, nombre: 'Proyecto 1', imagen: '/imagenes/proyecto1.png', link: '/proyecto1' },
+  { id: 2, nombre: 'Proyecto 2', imagen: '/imagenes/proyecto2.png', link: '/proyecto2' },
+  { id: 3, nombre: 'Proyecto 3', imagen: '/imagenes/proyecto3.png', link: '/proyecto3' },
+  { id: 4, nombre: 'Proyecto 4', imagen: '/imagenes/proyecto4.png', link: '/proyecto4' },
+  { id: 5, nombre: 'Proyecto 5', imagen: '/imagenes/proyecto5.png', link: '/proyecto5' }
+  
+];
+
+const indiceCentral = ref(2);
+
+const getStyle = (i: number) => {
+  const offset = i - indiceCentral.value;
+  const scale = Math.pow(0.8, Math.abs(offset));
+  const zIndex = 100 - Math.abs(offset);
+  const translateZ = -100 * offset;
+  const translateX = offset * 60;
+  return {
+    transform: `translateX(${translateX}px) translateZ(${translateZ}px) scale(${scale})`,
+    zIndex,
+  };
+};
+
+const calcularTranslateX = () => {
+  return 0;
+};
+
+const irA = (i: number) => {
+  indiceCentral.value = i;
+};
 
 </script>
 
@@ -67,36 +92,27 @@ const mouseLeave = (boton: any) => {
   <div class="contenedor">
     <div class="contenido">
       <div class="intro">
-        <p class="intro-text">nice to meet you, my name is Claudia, a soon to be illustrator</p>
-       
-       <picture class="hero-picture">
-        <source srcset="/imagenes/de-hecho-yo.png"
-        alt="yo"
-        class="hero-image"
-        >
 
-        <img src="/imagenes/de-hecho-yo.png" 
-        alt="yo"
-        class="hero-image"
-        >
-       </picture>
-    </div>
+        <div class="w-full h-96 flex justify-center items-center perspective-container overflow-hidden">
+          <div class="relative w-full h-full flex justify-center items-center">
+            <div class="flex relative transition-all dration-500"
+            :style="{ transform: `translateX(${calcularTranslateX()}px)` }">
+              <div
+              v-for="(proyecto, i) in proyectos"
+              :key="proyecto.id"
+              class="absolute top-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer transition-all duration-500"
+              >
+                <img :src="proyecto.imagen"
+                 :alt="proyecto.nombre"
+                 class="rounded-lg shadow-lg w-48 md:w-64"
+                 />
+              </div>
+            </div>
+          </div>
+        </div>
 
-
-    <div class="postit-grid">
-      <div
-      v-for="boton in botones"
-      :key="boton.id"
-      class="postit-button"
-      @mouseenter="mouseEnter(boton)"
-      @mouseleave="mouseLeave(boton)"
-      >
-    
-      <img :src="boton.current.value" alt="post-it">
 
     </div>
-    </div>
-
     </div>
   </div>
 
@@ -115,75 +131,16 @@ const mouseLeave = (boton: any) => {
 
 <style>
 
-.postit-grid {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  gap: 1rem;
-  margin-top: 2rem;
-  justify-content: center;
-  margin: auto;
+.perspective-container {
+  perspective: 1200px;
 }
 
-.postit-button img {
-  width: 100%;
-  max-width: 150px;
-  height: auto;
-  transition: all 0.3s ease;
-  display: block;
-  cursor: pointer;
-  margin: auto;
+img {
+  transition: transform 0.5s;
 }
 
-@media (min-width: 768px) {
-  .postit-grid {
-    grid-template-columns: repeat(3, 1fr);
-    gap: 1.5rem;
-    justify-content: center;
-  }
-  .postit-button img {
-    max-width: 180px;
-  }
-}
-
-.intro {
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  padding: clamp(4rem, 10vw, 8rem) clamp(2rem, 5vw, 6rem);
-  text-align: left;
-  align-items: center;
-  transition: padding 0.3s ease, gap 0.3s ease;
-  margin: auto;
-}
-
-.hero-picture img {
-  width: 100%;
-  max-width: 200px;
-  height: auto;
-  transition: all 0.3s ease;
-  display: block;  
-  margin: auto;
-}
-
-.intro > * {
-  width: 100%;
-  max-width: 400px;
-  transition: all 0.3s ease;
-}
-
-.hero-image {
-  width: 100%;
-  max-width: 200px;
-  height: auto;
-}
-
-.intro-text {
-  font-family: "AlteHaasGroteskBold", sans-serif;
-  font-size: 1rem;
-  color: #0a0d1c;
-  max-width: 150px;
-  line-height: 1.4;
-  margin: auto;
+img:hover {
+  transform: scale(1.05);
 }
 
 .icon-wrapper {
@@ -198,7 +155,7 @@ const mouseLeave = (boton: any) => {
   margin-left: 0.25rem;
   font-size: 0.75rem;
   font-family: "AlteHaasGroteskBold", sans-serif;
-  color: #0a0d1c;
+  color: #0e0d0c;
   padding: 0;
   background: none;
   opacity: 0;
@@ -216,7 +173,7 @@ const mouseLeave = (boton: any) => {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #fff9d6;
+  background-color: #eae9e5;
   padding: 0.5rem 2rem;
 }
 
@@ -230,21 +187,16 @@ const mouseLeave = (boton: any) => {
 }
 
 @media (min-width: 768px) {
-  .intro {
-    padding: clamp(6rem, 8vw, 10rem) clamp(3rem, 6vw, 8rem);
-  }
-  .hero-picture img {
-    max-width: 300px;
+  .menu-items {
+    gap: 2rem;
   }
 }
 
 @media (min-width: 1024px) {
-  .intro {
-    padding: clamp(6rem, 8vw, 10rem) clamp(4rem, 8vw, 10rem);
+  .menu-items {
+    gap: 3rem;
   }
 }
-
-
 
 .menu-button {
   background: none;
@@ -252,29 +204,29 @@ const mouseLeave = (boton: any) => {
   font-family: "AlteHaasGroteskBold", sans-serif;
   font-size: 0.875rem;
   cursor: pointer;
-  color: #0a0d1c;
+  color: #0e0d0c;
   padding: 0.25rem 0.5rem;
   border-radius: 999px;
   transition: color 0.3s ease-in-out, background-color 0.3s ease-in-out
 }
 
 .menu-button:hover {
-  color: #fff9d6;
-  background-color: #0a0d1c;
+  color: #eae9e5;
+  background-color: #0e0d0c;
 }
 
 .router-link-exact-active {
-  color: #fff9d6;
-  background-color: #0a0d1c;
+  color: #eae9e5;
+  background-color: #0e0d0c;
   transition: all 0.25s ease;
 }
 
 
 .contenedor {
   width: 100%;
-  padding: 3rem 2rem;
+  padding: 13rem 2rem;
   box-sizing: border-box;
-  background-color: #fff9d6;
+  background-color: #eae9e5;
   min-height: 100vh;
 }
 
@@ -285,13 +237,13 @@ const mouseLeave = (boton: any) => {
 
 @media (min-width: 768px) {
   .contenedor {
-    padding: 10rem 4rem;
+    padding: 13rem 4rem;
   }
 }
 
 @media (min-width: 1024px) {
   .contenedor {
-    padding: 8rem 6rem;
+    padding: 13rem 6rem;
   }
   
 }
